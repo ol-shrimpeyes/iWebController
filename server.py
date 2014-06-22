c841f2802d24352a3103e9f9914b7d4d9c400088
matthew masters <apt.1924@gmail.com>
2014-06-22 12:39:09 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import *
+# from sh import media
+from os import urandom
+
+app = Flask(__name__)
+app.secret_key = urandom(255)
+
+@app.route('/')
+def main():
+	return redirect(url_for('controls'))
+
+@app.route('/controls', methods=['GET', 'POST'])
+@app.route('/controls/<control>', methods=['GET', 'POST'])
+def controls(control=None):
+	if request.method == 'GET':
+		if control:
+			media(control)
+			return url_for('controls')
+		else:
+			return render_template('controls.html')
+	elif request.method == 'POST':
+		cont = request.form['control']
+		return redirect(url_for('controls', control=cont))
+
+app.run(debug=True)
