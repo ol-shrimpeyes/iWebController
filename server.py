f65f277e82f5efa863a9d28bec4785f98c97ab95
matthew masters <apt.1924@gmail.com>
2014-06-24 17:24:13 -0500

    bug fix

--- server.py
+++ server.py
@@ -9,6 +9,9 @@ app.secret_key = urandom(255)
 
 media_codes = [0,2,4,12,13]
 
+isPlaying
+isPaused
+
 @app.route('/')
 def main():
 	return redirect(url_for('controls'))
@@ -23,12 +26,12 @@ def controls(control=None):
 			isPaused = str(media('isPaused', _ok_code=media_codes))[0]
 			return redirect(url_for('controls'))
 		else:
-			return render_template('controls.html', now_playing=nowPlaying(isPaused, isPlaying))
+			return render_template('controls.html', now_playing=nowPlaying())
 	elif request.method == 'POST':
 		cont = request.form['control']
 		return redirect(url_for('controls', control=cont))
 
-def nowPlaying(isPaused,isPlaying):
+def nowPlaying():
 	if isPlaying == '1':
 		artist = str(media('artist', _ok_code=media_codes))
 		track = str(media('title', _ok_code=media_codes))
