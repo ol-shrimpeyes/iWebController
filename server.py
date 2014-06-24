0b47a5670ac50a4fa8c672d131bc51a9ac5d0d78
matthew masters <apt.1924@gmail.com>
2014-06-24 17:27:41 -0500

    reversed some changes

--- server.py
+++ server.py
@@ -2,15 +2,12 @@ from flask import *
 from sh import media
 from os import urandom
 from conf import *
-from time import sleep
+from tim
 
 app = Flask(__name__)
 app.secret_key = urandom(255)
 
 media_codes = [0,2,4,12,13]
-
-isPlaying = ''
-isPaused = ''
 
 @app.route('/')
 def main():
@@ -22,22 +19,20 @@ def controls(control=None):
 	if request.method == 'GET':
 		if control:
 			media(control)
-			
+			sleep(1)
 			return redirect(url_for('controls'))
 		else:
-			isPlaying = str(media('isPlaying', _ok_code=media_codes))[0]
-			isPaused = str(media('isPaused', _ok_code=media_codes))[0]
 			return render_template('controls.html', now_playing=nowPlaying())
 	elif request.method == 'POST':
 		cont = request.form['control']
 		return redirect(url_for('controls', control=cont))
 
 def nowPlaying():
-	if isPlaying == '1':
+	if str(media('isPlaying', _ok_code=media_codes))[0] == '1':
 		artist = str(media('artist', _ok_code=media_codes))
 		track = str(media('title', _ok_code=media_codes))
 		playing = track +'- '+ artist
-	elif isPaused == '1':
+	elif str(media('isPaused', _ok_code=media_codes))[0] == '1':
 		artist = str(media('artist', _ok_code=media_codes))
 		track = str(media('title', _ok_code=media_codes))
 		playing = Markup(track +'- '+ artist + '<em><strong>[paused]<strong></em>')
