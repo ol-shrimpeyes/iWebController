059207d023b07d395af8ebba8c9d91cc3339c49e
matthew masters <apt.1924@gmail.com>
2014-06-25 18:45:51 -0500

    added volume support

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import *
+tfrom flask import *
 from sh import media
 from os import urandom
 from conf import *
@@ -7,7 +7,7 @@ from time import sleep
 app = Flask(__name__)
 app.secret_key = urandom(255)
 
-media_codes = [0,2,4,12,13]
+media_codes = [0,2,4,12,13,23]
 
 @app.route('/')
 def main():
@@ -22,10 +22,16 @@ def controls(control=None):
 			sleep(1)
 			return redirect(url_for('controls'))
 		else:
-			return render_template('controls.html', now_playing=nowPlaying())
+			return render_template('controls.html', now_playing=nowPlaying(), volume=getVol())
 	elif request.method == 'POST':
 		cont = request.form['control']
 		return redirect(url_for('controls', control=cont))
+
+def getVol():
+	vol = media('getVol')
+	vol = str(int(float(vol)*100)) + '%'
+	return vol
+
 
 def nowPlaying():
 	isPlaying = str(media('isPlaying', _ok_code=media_codes))[0]
